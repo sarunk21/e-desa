0fbd71534b44740d0cb7ba9aec6beaeaac8810aa
sarunk <kautsarmkp@gmail.com>
2023-06-07 22:25:23 +0700

    fix https route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        if ($this->app->environment('production') || $this->app->environment('staging')) {
             URL::forceScheme('https');
         }
     }
