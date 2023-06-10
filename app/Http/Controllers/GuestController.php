0158ec1104f657d1e7aad53ff0d05232c03b1248
sarunk <kautsarmkp@gmail.com>
2023-06-10 21:43:40 +0700

    fix id null

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -8,14 +8,14 @@ class GuestController extends Controller
 {
     public function wellcome()
     {
-        $jumlah_notifikasi = Notifikasi::where('user_id', auth()->user()->id)->where('status_notifikasi', Notifikasi::STATUS_UNREAD)->count() ?? 0;
+        $jumlah_notifikasi = Notifikasi::where('user_id', auth()->user()->id ?? null)->where('status_notifikasi', Notifikasi::STATUS_UNREAD)->count() ?? 0;
 
         return view('wellcome', compact('jumlah_notifikasi'));
     }
 
     public function informasi()
     {
-        $jumlah_notifikasi = Notifikasi::where('user_id', auth()->user()->id)->where('status_notifikasi', Notifikasi::STATUS_UNREAD)->count() ?? 0;
+        $jumlah_notifikasi = Notifikasi::where('user_id', auth()->user()->id ?? null)->where('status_notifikasi', Notifikasi::STATUS_UNREAD)->count() ?? 0;
 
         return view('informasi', compact('jumlah_notifikasi'));
     }
